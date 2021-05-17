d2ccd6bc102f07fd948104fc3c291c6118d07cfe
MarcosRene <renemoura789@gmail.com>
2021-05-17 19:15:02 -0300

    add loading Button component

---
+++ src/components/Button/Spinner/index.tsx
@@ -0,0 +1,9 @@
+import './styles.scss';
+
+export const Spinner = () => {
+  return (
+    <svg className="spinner" viewBox="0 0 50 50">
+      <circle cx="25" cy="25" r="20" />
+    </svg>
+  );
+};
