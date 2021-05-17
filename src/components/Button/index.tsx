2b39b9efd20d74384ae6933a6ee251e98e5bdc39
MarcosRene <renemoura789@gmail.com>
2021-05-17 19:11:39 -0300

    new button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,22 +1,61 @@
-import { ButtonHTMLAttributes, PropsWithChildren } from 'react';
+import {
+  useState,
+  useEffect,
+  useRef,
+  ButtonHTMLAttributes,
+  PropsWithChildren,
+} from 'react';
 import cx from 'classnames';
+
+import { Spinner } from './Spinner';
 
 import styles from './styles.module.scss';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
-  variant: string;
-  size: string;
+  variant: 'primary' | 'secondary' | 'outline' | 'link';
+  size: 'small' | 'medium' | 'large';
   isLoading?: boolean;
 };
 
 export const Button = (props: PropsWithChildren<ButtonProps>) => {
+  const refButton = useRef<HTMLButtonElement>(null);
+
   const { className, variant, size, isLoading, children, ...rest } = props;
 
-  const _className = cx(className, styles.btn, styles[variant], styles[size]);
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    if (refButton.current && refButton.current.getBoundingClientRect().width) {
+      setWidth(refButton.current.getBoundingClientRect().width);
+    }
+    if (refButton.current && refButton.current.getBoundingClientRect().height) {
+      setHeight(refButton.current.getBoundingClientRect().height);
+    }
+  }, [children]);
+
+  const _className = cx(
+    styles.btn,
+    styles[variant],
+    styles[size],
+    `${isLoading}` ? { width, height } : {}
+  );
 
   return (
-    <button className={_className} {...rest}>
-      {children}
+    <button
+      {...rest}
+      ref={refButton}
+      className={_className}
+      style={
+        isLoading
+          ? {
+              width,
+              height,
+            }
+          : {}
+      }
+    >
+      {isLoading ? <Spinner /> : children}
     </button>
   );
 };
@@ -26,4 +65,6 @@ Button.defaultProps = {
   onClick: () => null,
   disabled: false,
   variant: 'basic',
+  size: 'medium',
+  isLoading: false,
 };
